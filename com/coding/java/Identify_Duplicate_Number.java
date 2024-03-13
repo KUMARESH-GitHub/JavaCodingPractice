6902e30f41a9b12bc3de3865770da696a0b19a6c
KUMARESH S <kumaresh.cs14@gmail.com>
2024-03-13 11:03:21 +0530

    commit 5

--- com/coding/java/Identify_Duplicate_Number.java
+++ com/coding/java/Identify_Duplicate_Number.java
@@ -1,10 +1,10 @@
 package com.coding.java;
 
-public class Duplicate_Number {
+public class Identify_Duplicate_Number {
 
     public static void main(String[] args)
     {
-        int given [] = {1,2,3,4,1,2,1,6,7,4,5,9};
+        int given [] = {1,2,3,4,1,8,1,6,7,4,5,9,3,1,7};
         int size = given.length;
 
         size = removeduplicates(given, size);
