989d460844b0c44ebf8438768dc9b04633f18dee
KUMARESH S <kumaresh.cs14@gmail.com>
2024-03-13 15:26:40 +0530

    commit 7

--- com/coding/java/RemoveWhiteSpaces_Without_ReplaceAll_Method.java
+++ com/coding/java/RemoveWhiteSpaces_Without_ReplaceAll_Method.java
@@ -1,7 +1,6 @@
 package com.coding.java;
 
 public class RemoveWhiteSpaces_Without_ReplaceAll_Method {
-
     public static void main(String[] args) {
         String str1 = "R  aga  vi Kum ar esh Ni mall an Aac hu tth an";
 
